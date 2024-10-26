a81c6d9b2a0c7fd934e9500abbbc968b9715577b
Diego Alejandro Regalado Segura <dregalados@miumg.edu.gt>
2024-10-26 14:01:54 -0600

    Add files via upload

---
+++ PatronMVC/src/main/java/mvc/View.java
@@ -0,0 +1,8 @@
+package mvc;
+
+public class View {
+    public void displayUser(String name, int age){
+        System.out.println("Name user: " + name);
+        System.out.println("Age user: "+ age);
+    }
+}
